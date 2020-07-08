f4545f1b8e415733b2183d067fba5eaf4c4ab61a
Amna Munir <amnamunir612@gmail.com>
2020-07-09 04:11:35 +0500

    API

---
+++ WebAPIMvc/WebAPIMvc/Controllers/EmployeesDataController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIMvc.Controllers
+{
+    public class EmployeesDataController : ApiController
+    {
+        //array get
+        public string[] myEmployees = { "Jibran ", "Ahmer", "Adeel" };
+         //[HttpGet] if not so run not necessary because bydefault getRequest
+         //[HttpGet] url in enter data larehai ho prefix Get convention any action method before convention Get
+         [HttpGet]
+        public string[] GetEmployees()
+        {
+            return myEmployees;
+        }
+
+        //string return
+        //parameter pass parameter name must id  in url controller kai baad
+        [HttpGet]
+        public string GetEmployeeByIndex(int id)
+        {
+            return myEmployees[id];
+        }
+    }
+}
